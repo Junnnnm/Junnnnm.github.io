e253c261d78f3ba8be97b51faefa0c8ee3fd9d81
Junnnnm <junyutang6@gmail.com>
2025-02-24 12:15:29 -0600

    draw

--- 05 Drawing with Single Loops/sketch.js
+++ 05 Drawing with Single Loops/sketch.js
@@ -1,9 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Drawing with Single Loops
+// Junyu
+// Feb 24
+// 
 
 
 function setup() {
@@ -11,5 +9,39 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  gradientBackground();
+  circleLine();
+
+  // screen gets updated here
+}
+
+function gradientBackground(){
+  // creat a gradient to use as background
+  let h = 10;
+
+  // use a loop to draw vertical stack of rectangles
+  for(let y = 0; y < height; y += h){
+    noStroke();
+    let mappedY = map(y,0,height,0,255);
+    let reversedY = map(y,0,height,255,0);
+    fill(mappedY, mappedY, mouseX);
+    rect(0, y, width, h);
+  }
+
+}
+
+function circleLine(){
+  //use a loop (for or while) to draw a line
+  //of circle side by side
+  let d = 30;   // diameter of each circle
+  let y = height/2;
+  let xStart = 0;
+  let xEnd = width;
+
+  //use a loop to do the drawing
+  for(let x = xStart; x <= xEnd; x+=d){
+    //x:0 40 80 120 160 200 240
+    circle(x, y, d);
+  }
+
 }
