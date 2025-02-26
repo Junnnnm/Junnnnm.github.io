317c18a59fd4e21e2316eebdfb520e9eced560ef
Junnnnm <junyutang6@gmail.com>
2025-02-26 12:17:13 -0600

    image deme done

---
+++ 06 Using images in p5/sketch.js
@@ -0,0 +1,61 @@
+// Image Basics
+// Mr.Scott
+// Feb 26, 2025
+
+// Global Variables
+let lionL, lionR;
+let pinImages = [];  // 0-8
+let currentFrame = 0;
+let facing = "left"; // "left" "right" 
+
+function preload(){
+  //function runs and won not end
+  //until all file loading is complete
+  lionL = loadImage("assets/lion-left.png");
+  lionR = loadImage("assets/lion-right.png");
+
+  for(let i = 0; i <= 8; i++){
+    pinImages.push(loadIamge("assets/pin-0" + 0 + ".png"));
+  }
+ 
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  frameRate(2);
+}
+
+function draw(){
+  background(220);
+
+
+  image(pinIamges[currentFrame], width/2, height/2);
+  if(frameCount % 3 === 0){
+    currentFrame++;
+  }
+  if(currentFrame > 8) currentFrame = 0;
+
+  drawLion();
+}
+ 
+
+function drawLion(){
+  //Alt-SHIFT-F(format) -- auto indentation
+  //Lion code
+  let sizeX = lionL.width / 2;
+  let sizeY = lionL.width / 2;
+
+  // managing the state variable
+  if (movedX > 0) facing = "right";
+  else if (movedX < 0) facing = "left";
+
+  //interpreting the state variable
+  if(facing === "left"){
+    image(lionL, mouseX, mouseY, sizeX, sizeY);
+  }
+  else{
+    image(lionR, mouseX, mouseY, sizeX, sizeY);
+  }
+
+}
